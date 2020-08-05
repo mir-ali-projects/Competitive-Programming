e4b6ae13ebfa967d7b38f65aef27e472e7d6d2d2
Mir Ali <66625456+mir-ali-projects@users.noreply.github.com>
2020-08-05 23:50:40 +0530

    Create JugglingAlgoRightArrayRotate.c

---
+++ JugglingAlgoRightArrayRotate.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/* function to print an array */
+void printArray(int arr[], int size);
+
+/*Fuction to get gcd of a and b*/
+int gcd(int a, int b);
+
+/*Function to left rotate arr[] of siz n by d*/
+void leftRotate(int arr[], int d, int n)
+{
+int i, j, k, temp,new_temp;
+/* To handle if d >= n */
+d = d % n;
+int g_c_d = gcd(d, n);
+for (i = 0; i < g_c_d; i++) {
+/* move i-th values of blocks */
+temp = arr[i];
+j = i;
+while (1) {
+k = (j + d) % n;
+
+if (k == i)
+break;
+new_temp= arr[k];
+arr[k] = temp;
+j = k;
+temp=new_temp;
+}
+arr[i] = temp;
+}
+}
+
+/*UTILITY FUNCTIONS*/
+/* function to print an array */
+void printArray(int arr[], int n)
+{
+int i;
+for (i = 0; i < n; i++)
+printf("%d ", arr[i]);
+}
+
+/*Fuction to get gcd of a and b*/
+int gcd(int a, int b)
+{
+if (b == 0)
+return a;
+else
+return gcd(b, a % b);
+}
+
+/* Driver program to test above functions */
+int main()
+{
+int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+leftRotate(arr, 2, 7);
+printArray(arr, 7);
+getchar();
+return 0;
+} 
